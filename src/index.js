3d38135010ba5e4c5b11d71829d5a91ae3f22083
Kyle Roush <roush.kyle@gmail.com>
2018-12-18 23:28:35 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 import Root from './Root'
 
+
 ReactDOM.render(<Root />, document.getElementById('root'));
 registerServiceWorker();
